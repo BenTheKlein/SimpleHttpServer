e43be9c4fa6e3a6fe2e46b978d34c8226515933b
Ben <>
2022-02-20 16:09:27 +0200

    clear

--- simple_server.py
+++ simple_server.py
@@ -14,6 +14,7 @@ class myHandler(BaseHTTPRequestHandler):
 			if "clear.sample.txt" in self.path:
 				import os
 				os.remove('sample.txt')
+				open('clear.sample.txt', 'a')
 		except:
 			pass
 
